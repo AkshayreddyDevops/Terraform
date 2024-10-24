d488f223c0e92a351951c8553e7752a157da7308
akga0822 <akshay.gaddam@netcracker.com>
2024-10-24 13:23:03 +0530

    tfimp-5

--- 01-resource/main.tf
+++ 01-resource/main.tf
@@ -1,20 +1,22 @@
 
 #Create EC2 instances
-resource "aws_instance" "frontend"{
-  instance_type = "t3.small"
+resource "aws_instance" "instance"{
+  for_each = var.components
+  instance_type = each.value["instance_type"]
   ami = data.aws_ami.ami.id
   vpc_security_group_ids = data.aws_security_groups.sg.ids
   tags ={
-    Name="frontend"
+    Name= "${each.key}"
   }
 }
 
 # Create Rout53
 
-resource "aws_route53_record" "frontend"{
+resource "aws_route53_record" "dns_record"{
+  for_each = var.components
   zone_id = data.aws_route53_zone.zoneid.zone_id
-  name = "frontend.dev.${var.domain_name}"
+  name = "${each.key}.dev.${var.domain_name}"
   type = "A"
   ttl = 300
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.instance[each.key].private_ip]
 }
--- 01-resource/variables.tf
+++ 01-resource/variables.tf
@@ -1,8 +1,14 @@
-variable "instalce_type" {
-  default = "t3.small"
-}
-
-
 variable "domain_name" {
   default = "eudtech.online"
+}
+
+variable "components"{
+  default = {
+    frontend ={
+      instalce_type = "t3.micro"
+    }
+    mongo ={
+      instalce_type = "t3.small"
+    }
+  }
 }
