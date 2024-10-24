29bd70469bf4eccbe4ec04e234f01fa7253a4cdd
akga0822 <akshay.gaddam@netcracker.com>
2024-10-24 07:42:42 +0530

    Terraform output

--- 01-resource/ec2.tf
+++ 01-resource/ec2.tf
@@ -1,7 +1,19 @@
-resource "aws_instance" "test"{
-  instance_type = "t3.micro"
+#Create EC2 instances
+resource "aws_instance" "frontend"{
+  instance_type = "t3.small"
   ami = "ami-09c813fb71547fc4f"
-  tags = {
-    Name = "Test"
+  vpc_security_group_ids = ["sg-0d2a27415e13203c0"]
+  tags ={
+    Name="frontend"
   }
+}
+
+# Create Rout53
+
+resource "aws_route53_record" "frontend"{
+  zone_id = "Z085093733LY1YYTUF9Q4"
+  name = "frontend.dev.edutech.online"
+  type = "A"
+  ttl = 300
+  records = [aws_instance.frontend.private_ip]
 }
