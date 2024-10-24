0c350f28075d8083b8e605f9ca67f6fc7e3dcdfb
akga0822 <akshay.gaddam@netcracker.com>
2024-10-24 08:12:09 +0530

    vars,list,map

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -2,6 +2,26 @@ variable "x"{
   default = 10
 }
 
+#list
+variable "list1" {
+  default = ["DevOps","6AM","Kumar"]
+}
+
+variable "map1" {
+  default = {
+    course = "DevOps"
+    tranner = "Kumar"
+    time = "6pm"
+  }
+}
 output "x"{
   value = var.x
+}
+
+output "list1"{
+  value = "Training ${list1[0]} starts at ${list1[1]} tought by ${list1[2]}}"
+}
+
+output "map1" {
+  value = "Traning ${map1["course"]} starts at ${map1["time"]} tought by ${map1["tranner"]}}"
 }
