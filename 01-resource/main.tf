d0a4f5b733fa7d37d7bc081c057689968905e082
akga0822 <akshay.gaddam@netcracker.com>
2024-10-24 12:20:37 +0530

    tfimp-4

--- 01-resource/main.tf
+++ 01-resource/main.tf
@@ -2,7 +2,7 @@
 #Create EC2 instances
 resource "aws_instance" "frontend"{
   instance_type = "t3.small"
-  ami = data.aws_ami.ami
+  ami = data.aws_ami.ami.id
   vpc_security_group_ids = data.aws_security_groups.sg.ids
   tags ={
     Name="frontend"
@@ -12,7 +12,7 @@ resource "aws_instance" "frontend"{
 # Create Rout53
 
 resource "aws_route53_record" "frontend"{
-  zone_id = data.aws_route53_zone.zoneid
+  zone_id = data.aws_route53_zone.zoneid.id
   name = "frontend.dev.${var.domain_name}"
   type = "A"
   ttl = 300
