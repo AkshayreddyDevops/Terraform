baaa9be3a8a39938e6c4fbdd0f5ab141decba766
akga0822 <akshay.gaddam@netcracker.com>
2024-10-24 14:35:08 +0530

    tfimp-6

--- 02-DNS-test/main.tf
+++ 02-DNS-test/main.tf
@@ -2,6 +2,8 @@ resource "aws_route53_record" "test" {
   zone_id = "Z085093733LY1YYTUF9Q4"
   name = "test"
   type = "A"
-  ttl = 300
+  ttl = var.ttl
   records = ["1.1.1.1"]
 }
+
+variable "ttl" {}
